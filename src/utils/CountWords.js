607843089114f78474e8eb5da28d9fe5a3f5ac4c
kensylee <kingsleyumeze@yahoo.com>
2024-05-16 12:02:33 +0100

    changes

--- src/utils/CountWords.js
+++
@@ -1,6 +0,0 @@
-// Function to count words
-function countWords(text) {
-    formattedText = text.split(/\s+/);
-    return formattedText.length;
-}
-module.exports=countWords;
--- src/utils/FormatReadingTime.js
+++
@@ -1,7 +0,0 @@
-
-// Function to format reading time in a more readable form
-function formatReadingTime(minutes){
-    const formattedTime = `${minutes} Minutes`;
-    return formattedTime;
-}
-module.exports=formatReadingTime;
